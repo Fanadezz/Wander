934976f88758150bb5c3b96109b9bbedb8d2fa6b
Antony <vontonnie@gmail.com>
2021-02-14 18:06:37 +0300

    Add Snippet

--- app/src/main/java/com/androidshowtime/MapsActivity.kt
+++ app/src/main/java/com/androidshowtime/MapsActivity.kt
@@ -10,6 +10,7 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import java.util.*
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -21,6 +22,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+
     }
 
     /**
@@ -32,10 +35,10 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
+
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
-
-
 
         val lat = -1.298311703512306
         val lng = 36.76229638360123
@@ -43,50 +46,79 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         val zoomLevel = 17f
 
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(homeLatLng,zoomLevel) )
-
-        map.addMarker(MarkerOptions().position(homeLatLng))
-
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-
-        menuInflater.inflate(R.menu.menu, menu)
-
-        return super.onCreateOptionsMenu(menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-
-        //super.onOptionsItemSelected(item)
-        return when (item.itemId) {
-
-            R.id.normal_map -> {
-                map.mapType = GoogleMap.MAP_TYPE_NORMAL
-
-                true
-            }
-            R.id.hybrid -> {
-
-                map.mapType = GoogleMap.MAP_TYPE_HYBRID
-                true
-            }
-
-            R.id.satellite_map -> {
-
-                map.mapType = GoogleMap.MAP_TYPE_SATELLITE
-                true
-            }
 
 
-            R.id.terrain_map -> {
-                map.mapType = GoogleMap.MAP_TYPE_TERRAIN
-                true
-            }
+        //move camera with zoom
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(homeLatLng, zoomLevel))
 
-            else               -> super.onOptionsItemSelected(item)
+
+
+
+
+
+
+    map.addMarker(MarkerOptions().position(homeLatLng))
+
+    //add long press marker
+    addMarkerOnLongPress(map)
+
+}
+
+override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+    menuInflater.inflate(R.menu.menu, menu)
+
+    return super.onCreateOptionsMenu(menu)
+}
+
+override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+    //super.onOptionsItemSelected(item)
+    return when (item.itemId) {
+
+        R.id.normal_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_NORMAL
+
+            true
         }
-    }
+        R.id.hybrid -> {
 
-    
+            map.mapType = GoogleMap.MAP_TYPE_HYBRID
+            true
+        }
+
+        R.id.satellite_map -> {
+
+            map.mapType = GoogleMap.MAP_TYPE_SATELLITE
+            true
+        }
+
+
+        R.id.terrain_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_TERRAIN
+            true
+        }
+
+        else               -> super.onOptionsItemSelected(item)
+    }
+}
+
+
+private fun addMarkerOnLongPress(map: GoogleMap) {
+
+    map.setOnMapLongClickListener {
+
+        val snippet =
+                String.format(Locale.getDefault(),
+                        "Lat: %1$.5f, Long: %2$.5f",
+                        it.latitude, it.longitude)
+
+        //use snippet() call on the
+        map.addMarker(MarkerOptions().position(it)
+                .title(getString(R.string.dropped_pin))// set the title
+                .snippet(snippet)) //set info
+    }
+}
+
+
 }
